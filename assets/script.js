707cafcf0d813411988cc4568705cf3ab78da894
SurosRegime <ephrain926@hotmail.com>
2023-03-31 01:32:33 -0400

    +
    linked html

--- assets/script.js
+++ assets/script.js
@@ -16,32 +16,49 @@
 
 //Get references to the #generate element
 var generateBtn = document.querySelector("#generate")
-var password = document.querySelector("#password");
 
+// Accepted characters
 var lowercaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numericalChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialChar = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+var possibleChar = [];
 
 //Write pw to the #password input
-function generatePassword() {
-    console.log("You clicked the button")
-}
-
+function writePassword() {
     var password = generatePassword();
-    var passwordChar = document.querySelector("#password");
- 
-if(userChoice >128 || userChoice <8){
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
+  
+//Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
+
+
+    // Clear previous password from screen
+    //password.value === "";
+    //var randomString = "";
+
+ // Choose password criteria
+ var hasLower = confirm("Do you want your password to contain lowercase letters?");
+ var hasUpper = confirm("Do you want your password to contain uppercase letters?");
+ var hasNum = confirm("Do you want your password to contain numbers?");
+ var hasSpec = confirm("Do you want your password to contain special characters?");
+ var passwordChar = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
+
+//Check if right length 
+if(passwordChar >128 || passwordChar <8){
     alert('Must be BETWEEN 8 & 128 characters')
     var passwordChar = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
+    
 }
  // Ensure that if someone clicks cancel for all password criteria that it asks them to choose at least one
- else if (lowercaseChar === false && uppercaseChar === false && num === false && specialChar === false) {
+ else if (hasLower === false && hasUpper === false && hasNum === false && hasSpec === false) {
     alert("You must chose at least one password criteria.");
-    var lowercaseChar = confirm("Do you want your password to contain lowercase letters?");
-    var uppercaseChar = confirm("Do you want your password to contain uppercase letters?");
-    var num = confirm("Do you want your password to contain numbers?");
-    var specialChar = confirm("Do you want your password to contain special characters?");
+    var hasLower = confirm("Do you want your password to contain lowercase letters?");
+    var uhasUpper = confirm("Do you want your password to contain uppercase letters?");
+    var hasNum = confirm("Do you want your password to contain numbers?");
+    var hasSpec = confirm("Do you want your password to contain special characters?");
 }
 
 // Use the criteria that are selected to generate the password
@@ -49,15 +66,15 @@ if (lowercaseChar) {
     optionsVariable += lowercaseAlphabet;
 }
 
-if (uppercaseChar) {
+if (hasUpper) {
     optionsVariable += uppercaseAlphabet;
 }
 
-if (num) {
+if (numericalChar) {
     optionsVariable += numbers;
 }
 
-if (specialChar) {
+if (hasSpec) {
     optionsVariable += specialCharacters;
 }
  // Generates a random password based on the criteria that have been selected
@@ -67,6 +84,3 @@ if (specialChar) {
 }
 password.value = randomString;
 
-//Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
-console.log()
